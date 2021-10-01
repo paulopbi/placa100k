74727f8bd5194f992dbd7351b99675acd22b5feb
Paulpbi <you@example.com>
2021-10-01 17:52:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Placa 100k
+
+
+Placa de 100k do youtube que permite que o usuário altere o nome do canal. Além disso, esse projeto ainda possui um “easter egg” quando o usuário clica no botão do youtube.
+
+
+![Preview](/preview.png)
+
+
+## O que eu aprendi durante esse projeto:
+- HTML
+  
+- CSS
+  - Variáveis CSS
+  - Unidades CSS (rem, %)
+  - Metologia BEM
+  - Diferença entre a propriedade box-shadow e o filtro drop-shadow
+  - Flexbox
+
+- Javascript
+  - .querySelector()
+  - .addEventListener()
+  - .add()
+  - .remove()
+  - .contains()
+
+E muito mais.
+
+### Minhas redes sociais:
+
+- [Instagram](https://www.instagram.com/paulopbi_/)
+  
+- [Facebook](https://www.facebook.com/profile.php?id=100005676409318)
+  
+- [Linkedin](https://www.linkedin.com/in/paulo-victor-silva-rosa-2179a8215/)
---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+const _elements = {
+	plaque: document.querySelector(".plaque"),
+	button: document.querySelector(".button"),
+	buttonPlay: document.querySelector(".button__play"),
+	buttonRightBroken: document.querySelector(".button--right-broken"),
+	channelNameInput: document.querySelector(".channel-name__input"),
+	channelNameDescription: document.querySelector(".description__channel-name")
+}
+
+_elements.channelNameDescription.addEventListener("input", () => {
+	_elements.channelNameInput.value = _elements.channelNameDescription.innerText;
+});
+
+_elements.channelNameInput.addEventListener("input", (e) => {
+	_elements.channelNameDescription.innerText = _elements.channelNameInput.value
+});
+
+_elements.button.addEventListener("click", () => {
+	if(_elements.button.classList.contains("button--broken"))
+		return;
+	inks.fx.shake(_elements.plaque, 15, false).then(brakePlaque);
+});
+
+const brakePlaque = () => {
+	_elements.button.classList.add("button--broken", "button--left-broken");
+	_elements.buttonPlay.classList.add("button__play--left-broken")
+	_elements.buttonRightBroken.classList.remove("button--hide")
+}
+
+_elements.channelNameInput.value = _elements.channelNameDescription.innerText;
